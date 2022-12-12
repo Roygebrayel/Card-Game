4ff9ab08aaa61ec2c2af28e7276b4742c14535e8
User <User@LAPTOP-3LMSPO9B>
2022-12-12 20:46:06 +0200

    s

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main(){
     cout<<"This game will auto play a game of likha between 4 bots and will outcome one loser";
     Player p1,p2,p3,p4;
-        List PlayerList;
-        p1 = initializePlayer(p1,"sergio",0);
-        p2 = initializePlayer(p2,"abbas",0);
-        p3 = initializePlayer(p3,"roy",0);
-        p4 = initializePlayer(p4,"georges",0);
+    List PlayerList;
+    p1 = initializePlayer(p1,"sergio",0);
+    p2 = initializePlayer(p2,"abbas",0);
+    p3 = initializePlayer(p3,"roy",0);
+    p4 = initializePlayer(p4,"georges",0);
 
-        initialize(PlayerList);
+    initialize(PlayerList);
         
-        AddPlayersInList(p1,p2,p3,p4,PlayerList);
-        printH2R(PlayerList);
+    AddPlayersInList(p1,p2,p3,p4,PlayerList);
+    printH2R(PlayerList);
 }
